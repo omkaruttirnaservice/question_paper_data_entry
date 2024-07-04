ed52b3ababd78bc2cf7eb56429c94f4cb236256c
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2024-07-04 18:16:42 +0530

    Updated: UI refactoring

--- web/src/components/MenuBar/MenuBar.js
+++ web/src/components/MenuBar/MenuBar.js
@@ -29,8 +29,7 @@ function MenuBar() {
 							to={el.path}
 							className={({ isActive }) =>
 								isActive ? 'menu-item active' : 'menu-item'
-							}
-						>
+							}>
 							<i className={el.icon}></i>
 							<span>{el.name}</span>
 						</NavLink>
