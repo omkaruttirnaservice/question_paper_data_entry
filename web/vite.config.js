11abc1a9713f663721bd78629d8f43deed02dd98
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2024-08-03 11:08:59 +0530

    vite config updated

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const mode = 'production';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:3026/',
+				target: mode == 'developement' ? 'http://localhost:3026/' : 'http://64.227.149.129:3026',
 				changeOrigin: true,
 			},
 		},
