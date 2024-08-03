d96ce7227b6512ca81f83377ac5f9fda5bf1c27f
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2024-08-03 10:30:55 +0530

    Ecosystem config file changes

--- api/ecosystem.config.cjs
+++ api/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'QP-data-entry-API',
-			script: './bin/www',
+			script: 'app.js',
 
 			// Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
 			instances: 1,
